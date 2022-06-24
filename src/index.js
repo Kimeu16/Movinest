9624175e8426c09d9c09871d9e3275cf0474a1bb
Anthony Kimeu <tkymeu@gmail.com>
2022-06-24 14:12:50 +0300

    Added function to js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ const search = document.getElementById("search");
 const APIURL = "https://api.themoviedb.org/3/discover/movie?api_key=04c35731a5ee918f014970082a0088b1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+// initially get fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+const resp = await fetch(url);
+const respData = await resp.json();
+
+console.log(respData);
+
+showMovies(respData.results);
+}
